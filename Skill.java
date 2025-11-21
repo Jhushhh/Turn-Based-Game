7108b78a0503870d48640616897f70a1421c0361
Rex III L. Enriquez <rexenriquez2005@gmail.com>
2025-11-21 09:42:02 +0800

    removed unnecessary override attack in hero subclasses

--- Skill.java
+++ Skill.java
@@ -2,12 +2,14 @@ public class Skill {
     String name;
     int manacomp;
     int damage;
-    public Skill(String name, int damage, int manacomp){
+
+    public Skill(String name, int damage, int manacomp) {
         this.name = name;
         this.manacomp = manacomp;
         this.damage = damage;
     }
-     public String getSkill_Name() {
+
+    public String getSkill_Name() {
         return name;
     }
 
--- Tank.java
+++ Tank.java
@@ -4,34 +4,26 @@ class Tank extends Hero {
 
     public Tank(String name) {
         super(name, default_hp, default_mp);
-        skill = new Skill[]{
-            new Skill("Shield Bash", 20, 10),
-            new Skill("Ground Slam", 30, 20),
-            new Skill("Iron Fist", 40, 30)  
+        skill = new Skill[] {
+                new Skill("Shield Bash", 20, 10),
+                new Skill("Ground Slam", 30, 20),
+                new Skill("Iron Fist", 40, 30)
         };
     }
 
-    @Override
-    public void attack(int skillIndex, Hero target) {
-        super.attack(skillIndex, target); 
-    }
-    
-    public String[] getSkill(){
-        return new String[]{
-            skill[0].getSkill_Name(),
-            skill[1].getSkill_Name(),
-            skill[2].getSkill_Name()
+    public String[] getSkill() {
+        return new String[] {
+                skill[0].getSkill_Name(),
+                skill[1].getSkill_Name(),
+                skill[2].getSkill_Name()
         };
     }
-    
+
     public Skill[] getSkillsArray() {
         return skill;
     }
-    
-    
-    
+
     public boolean isTank() {
         return true;
     }
 }
-
